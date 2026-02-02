30576d728020b56acf1aba233b802751c65c8075
Damini <daminichandraprabha15@gmail.com>
2026-02-02 23:03:42 +0530

    added frequency column

--- src/main/java/com/example/ShortURL/model/Chotaurl.java
+++ src/main/java/com/example/ShortURL/model/Chotaurl.java
@@ -15,6 +15,9 @@ public class Chotaurl {
     @Column(name="long_url")
     private String longURL;
 
+    @Column(name="frequency")
+    private int count=0;
+
     @Column(name="created_at")
     private Date createdAt;
 
@@ -49,6 +52,14 @@ public class Chotaurl {
 
     public void setLongURL(String longURL) {
         this.longURL = longURL;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
     }
 
     public Date getCreatedAt() {
