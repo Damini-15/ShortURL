68af6e11e1cd24d3a961449f0ea373365af37c91
Damini <daminichandraprabha15@gmail.com>
2026-02-02 22:50:40 +0530

    added API for redirection

--- src/main/java/com/example/ShortURL/controller/Ctrl.java
+++ src/main/java/com/example/ShortURL/controller/Ctrl.java
@@ -3,8 +3,11 @@ package com.example.ShortURL.controller;
 import com.example.ShortURL.model.Chotaurl;
 import com.example.ShortURL.service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -44,4 +47,20 @@ public class Ctrl {
         urlService.delete(id);
         return "Deleted";
     }
+
+    @GetMapping("chotaURL/{id}")
+    public ResponseEntity<Void> redirect(@PathVariable(value="id") long id){
+        Chotaurl chotaurl=urlService.readByID(id);
+        String longUrl = chotaurl.getLongURL();
+        if (!longUrl.startsWith("http://") && !longUrl.startsWith("https://")) {
+            longUrl = "https://" + longUrl;
+        }
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .location(URI.create(longUrl))
+                .build();
+
+    }
+
+
 }
